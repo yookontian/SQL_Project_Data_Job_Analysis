bb72b9e613c0559cfb0d75aa0ed81cda1b1bed2a
Tian-Laptop <jiayuetian@foxmail.com>
2024-04-03 19:13:15 +0900

    find the most in-demand skills

---
+++ project_sql/3_top_demanded_skills.sql
@@ -0,0 +1,22 @@
+/*
+Question: What are the most in-demand skills for data analysts?
+- Join job postings to inner join table similar to query 2
+- Identify the top 5 in-demand skills for a data analyst.
+- Focus on all job postings.
+- Why? Retrieves the top 5 skills with the highest demand in the job market, Providing insights into the most valuable skills for job seekers
+*/
+
+SELECT
+    skills,
+    COUNT(skills_job_dim.job_id) AS demand_count
+FROM job_postings_fact
+JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+WHERE
+    job_postings_fact.job_title_short = 'Data Analyst'
+    AND job_location = 'Anywhere'
+GROUP BY
+    skills
+ORDER BY
+    demand_count DESC
+LIMIT 5
